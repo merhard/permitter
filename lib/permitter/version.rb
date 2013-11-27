1109f24c8d39cf6729153729cb141aeb42da355b
Matt Erhard <matthew.erhard@egov.com>
2013-11-27 10:23:17 -0500

    initial commit

---
+++ lib/permitter/version.rb
@@ -0,0 +1,3 @@
+module Permitter
+  VERSION = "0.0.1"
+end
---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = 'ef39aa9d0d6c731b2edfe9ffabd47dc3e4255e3756d801df7863e18d254a4a34e7aab237108e13f2ae29e9db90557dcacf2f3c9e174f67db3a665b7348b253fc'
