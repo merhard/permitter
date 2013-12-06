2699d696083e0958b268e35e8965b16b4f3138b3
Matt Erhard <matthew.erhard@egov.com>
2013-12-06 09:53:12 -0500

    fix typo in matchers
    - 'allow' is now 'allowed'

--- lib/permitter/matchers.rb
+++ lib/permitter/matchers.rb
@@ -1,11 +1,11 @@
 RSpec::Matchers.define :allow_action do |*args|
   match do |permission|
-    expect(permission.allow_action?(*args)).to be true
+    expect(permission.allowed_action?(*args)).to be true
   end
 end
 
 RSpec::Matchers.define :allow_param do |*args|
   match do |permission|
-    expect(permission.allow_action?(*args)).to be true
+    expect(permission.allowed_action?(*args)).to be true
   end
 end
