598f819cccfb7d9a149ba74735a528df405fedb7
Matt Erhard <matthew.erhard@egov.com>
2013-12-06 16:18:35 -0500

    fixed specs for controller additions

--- spec/permitter/controller_additions_spec.rb
+++ spec/permitter/controller_additions_spec.rb
@@ -13,27 +13,30 @@ end
 describe Permitter::ControllerAdditions do
   before(:each) do
     @params = HashWithIndifferentAccess.new
+    @current_user = nil
+
     @controller_class = Class.new
     @controller = @controller_class.new
+
     @controller.stub(:params) { @params }
-    @controller.stub(:current_user) { nil }
     @controller_class.stub(:helper_method) { :helper_method }
+    @controller.stub(:current_permissions) { StubbedPermission.new(@current_user) }
+
     @controller_class.send(:include, Permitter::ControllerAdditions)
-    double(Permission) { StubbedPermission }
   end
 
   it "provides an allowed_action? method which goes through the current permissions" do
-    # expect(@controller.allowed_action?(:foo, :bar)).to be true
+    expect(@controller.allowed_action?(:foo, :bar)).to be true
     expect(@controller.allowed_action?(:foo, :baz)).to be false
   end
 
   it "authorize_user! should raise Unauthorized when user not authoried" do
     authorization = ->(action) do
-      @params.merge!(controller: "foo", action: "bar")
+      @params.merge!(controller: "foo", action: action)
       @controller.instance_eval{ authorize_user! }
     end
 
-    # expect(-> {authorization.call('bar')}).to_not raise_error
+    expect(-> {authorization.call('bar')}).to_not raise_error
     expect(-> {authorization.call('baz')}).to raise_error Permitter::Unauthorized
   end
 
